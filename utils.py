04ae1c438c44223a016721edb71c16006ad09542
The_Stallion <111058733+NotTheStallion@users.noreply.github.com>
2025-01-24 12:28:53 +0100

    Adidng everything

--- utils.py
+++ utils.py
@@ -88,7 +88,7 @@ def get_object_crops(frames, saliency_dir):
             continue
 
         saliency_map = cv2.imread(saliency_path, cv2.IMREAD_GRAYSCALE)
-        _, thresh = cv2.threshold(saliency_map, 128, 255, cv2.THRESH_BINARY)
+        _, thresh = cv2.threshold(saliency_map, 0, 255, cv2.THRESH_BINARY)
         contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         for contour in contours:
@@ -97,3 +97,58 @@ def get_object_crops(frames, saliency_dir):
             object_crops.append(crop)
 
     return object_crops
+
+
+
+
+def get_all_directories(root, objects):
+    directories = {}
+    for obj in objects:
+        obj_path = os.path.join(root, obj)
+        if os.path.isdir(obj_path):
+            directories[obj] = [os.path.join(obj_path, d) for d in os.listdir(obj_path) if os.path.isdir(os.path.join(obj_path, d))]
+    return directories
+
+
+def get_mp4_paths(directories):
+    mp4_paths = {}
+    for obj, dirs in directories.items():
+        mp4_paths[obj] = []
+        for dir in dirs:
+            for file in os.listdir(dir):
+                if file.endswith(".mp4"):
+                    mp4_paths[obj].append(os.path.join(dir, file))
+    return mp4_paths
+
+
+@energy_profiler
+def create_data(set="train"):
+    root = os.path.join("GITW_light", set)
+    directories = get_all_directories(root, OBJECTS)
+    mp4_paths = get_mp4_paths(directories)
+
+    for obj, paths in mp4_paths.items():
+        for path in paths:
+            # print(path)
+            frames = load_video_frames(path)
+            # print(f"Number of frames: {len(frames)}")
+            saliency_dir = os.path.join(os.path.dirname(path), "SaliencyMaps")
+            object_crops = get_object_crops(frames, saliency_dir)
+            # print(f"Number of object crops: {len(object_crops)}")
+            
+            
+            save_dir = os.path.join(os.path.join("data", set), obj)
+            if not os.path.exists(save_dir):
+                os.makedirs(save_dir)
+            for i, crop in enumerate(object_crops):
+                save_path = os.path.join(save_dir, f"{os.path.splitext(os.path.basename(path))[0]}_crop_{i}.jpg")
+                save_image(crop, save_path)
+
+
+
+OBJECTS = ["Bowl", "CanOfCocaCola", "Jam", "MilkBottle", "Mug", "OilBottle", "Rice", "Sugar", "VinegarBottle"]
+
+
+if __name__ == "__main__":
+    create_data("train")
+    create_data("test")
